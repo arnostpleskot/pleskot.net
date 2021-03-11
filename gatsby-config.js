b6b31adde0db3c212558d910b5ca0cc1e57e9f1e
Arnost Pleskot <arnostpleskot@gmail.com>
2021-03-11 17:48:09 +0100

    Get rid of google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,17 +45,5 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-34658474-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-      },
-    },
   ],
 }
